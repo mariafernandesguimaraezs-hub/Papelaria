76dc001678691d7702e9cd213e50a98b105bd7c6
mariafernandesguimaraezs <mariafernandesguimaraezs@gmail.com>
2026-08-28 16:56:31 -0300

    Tabelas

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=255)
+    ativa = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
+    
